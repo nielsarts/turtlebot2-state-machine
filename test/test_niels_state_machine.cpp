dc434710ad9608e62e7a26ed46ab70fb1724153c
narts <niels.p.arts@gmail.com>
2019-02-04 17:15:12 +0100

    FIX 1

--- test/test_niels_state_machine.cpp
+++ test/test_niels_state_machine.cpp
@@ -17,7 +17,6 @@ TEST(TestSuite, testCase2)
 // Run all the tests that were declared with TEST()
 int main(int argc, char **argv){
   testing::InitGoogleTest(&argc, argv);
-  ros::init(argc, argv, "tester");
-  ros::NodeHandle nh;
+  
   return RUN_ALL_TESTS();
 }
