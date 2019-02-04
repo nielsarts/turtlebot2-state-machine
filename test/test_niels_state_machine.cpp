55692b06ff6528b779902f5fa14cd5719c247fc4
narts <niels.p.arts@gmail.com>
2019-02-04 17:07:27 +0100

    FIX 1

--- test/test_niels_state_machine.cpp
+++ test/test_niels_state_machine.cpp
@@ -1,48 +1,24 @@
-#include "math_utils/MathExpression.h"
-#include "math_utils/math_utils.h"
+// Bring in my package's API, which is what I'm testing
+#include "foo/foo.h"
+// Bring in gtest
 #include <gtest/gtest.h>
 
-#define TEST_EXPRESSION(a) EXPECT_EQ((a), meval::EvaluateMathExpression(#a))
+// Declare a test
+TEST(TestSuite, testCase1)
+{
 
-TEST(MathExpressions, operatorRecognition){
-EXPECT_TRUE(meval::ContainsOperators("+"));
-EXPECT_TRUE(meval::ContainsOperators("-"));
-EXPECT_TRUE(meval::ContainsOperators("/"));
-EXPECT_TRUE(meval::ContainsOperators("*"));
-EXPECT_FALSE(meval::ContainsOperators("1234567890qwertyuiop[]asdfghjkl;'zxcvbnm,._=?8"));
 }
 
-TEST(MathExpressions, basicOperations){
-EXPECT_EQ(5, meval::EvaluateMathExpression("2+3"));
-EXPECT_EQ(5, meval::EvaluateMathExpression("2 + 3"));
-EXPECT_EQ(10, meval::EvaluateMathExpression("20/2"));
-EXPECT_EQ(-4, meval::EvaluateMathExpression("6 - 10"));
-EXPECT_EQ(24, meval::EvaluateMathExpression("6 * 4"));
+// Declare another test
+TEST(TestSuite, testCase2)
+{
+
 }
 
-TEST(MathExpressions, complexOperations){
-TEST_EXPRESSION(((3 + 4) / 2.0) + 10);
-TEST_EXPRESSION(7 * (1 + 2 + 3 - 2 + 3.4) / 12.7);
-TEST_EXPRESSION((1 + 2 + 3) - (8.0 / 10));
-}
-
-TEST(MathExpressions, UnaryMinus){
-TEST_EXPRESSION(-5);
-}
-
-TEST(MathExpressions, badInput){
-//TODO - figure out what good error behavior is and test for it properly
-//EXPECT_EQ(0, meval::EvaluateMathExpression("4.1.3 - 4.1"));
-//EXPECT_EQ(0, meval::EvaluateMathExpression("4.1.3"));
-}
-
-TEST(MathUtils, basicOperations){
-EXPECT_EQ(math_utils::clamp<int>(-10, 10, 20), 10);
-EXPECT_EQ(math_utils::clamp<int>(15, 10, 20), 15);
-EXPECT_EQ(math_utils::clamp<int>(25, 10, 20), 20);
-}
-
+// Run all the tests that were declared with TEST()
 int main(int argc, char **argv){
   testing::InitGoogleTest(&argc, argv);
+  ros::init(argc, argv, "tester");
+  ros::NodeHandle nh;
   return RUN_ALL_TESTS();
 }
