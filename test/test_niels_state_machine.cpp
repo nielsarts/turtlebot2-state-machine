78cd0564820e6bdaedcaf2905b9e1332bdabb1dc
narts <niels.p.arts@gmail.com>
2019-02-04 19:11:12 +0100

    FIX 2

--- test/test_niels_state_machine.cpp
+++ test/test_niels_state_machine.cpp
@@ -17,6 +17,6 @@ TEST(TestSuite, testCase2)
 // Run all the tests that were declared with TEST()
 int main(int argc, char **argv){
   testing::InitGoogleTest(&argc, argv);
-  
+
   return RUN_ALL_TESTS();
 }
